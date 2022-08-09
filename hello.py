9b28fa3949210e967b75e0135e012c1f8e80b221
Adoye <adoye123@gmail.com>
2022-08-09 18:26:26 +0000

    a

--- hello.py
+++ hello.py
@@ -1,4 +1,13 @@
+from ast import Sub
+from email.policy import default
+from enum import unique
+import string
+from venv import create
+from wsgiref.validate import validator
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -6,20 +15,39 @@ from datetime import datetime
 app = Flask(__name__)
 # app.config['TESTING'] = True
 # add db
+app.config['SECRET_KEY'] = "mykey"
+
+# Creaate a form class
+class NamerForm(FlaskForm):
+      name = StringField("What is your name", validators=[DataRequired()])
+      submit =SubmitField("Submit")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 # initialize the db
 db = SQLAlchemy(app)
 
 # create model
 class Users(db.Model):
-      id = db.Column()
-      name = db.Column()
-      email = db.Column()
-      date_added = db.Column()
+      id = db.Column(db.Integer, primary_key=True)
+      name = db.Column(db.String(200), nullable=False)
+      email = db.Column(db.String(120), nullable=False, unique=True)
+      date_added = db.Column(db.DateTime, default=datetime.utcnow)
       
+      # create a string
+      def __repr__(self):
+           return '<Name %r>' % self.name
       
 # Create a route decorator
-@app.route("/")
+@app.route('/')
 
 def index():
     return render_template("index.html")   
+
+@app.route('/shop')
+
+def shop():
+     return render_template("shop.html")
+
+@app.route('/contact')
+
+def contact():
+     return render_template("contact.html")
